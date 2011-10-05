0a8703fd9069916bdf5154b442e5561eaf3a1c46
Jimmy Bhullar <jimmybhullar26@gmail.com>
2011-10-05 14:12:13 -0700

    argv

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php
-echo "hello world!\n";
+
+echo ("argc = ".$argc ."\n\n");
+echo ("1: ".$argv[4] ."\n");
+echo ("2: ".$argv[3] ."\n");
+echo ("3: ".$argv[2] ."\n");
+echo ("4: ".$argv[1] ."\n");
+
 ?>
